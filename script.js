06d5d3a2581a437faef9c35de67c993c8ab03450
kvadratser1y <kvadratseriy@icloud.com>
2024-09-18 16:16:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -53,4 +53,3 @@ setInterval(() => {
         resetTimerElement.textContent = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
     }
 }, 1000);
-
